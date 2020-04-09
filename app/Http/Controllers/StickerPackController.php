0c7b3553ffd39155deea1088f81ece5cc1937f81
Kajemul Haque <kajemsust@gmail.com>
2020-04-09 16:15:01 +0600

    pack images are resizing when displaying in the website.

--- app/Http/Controllers/StickerPackController.php
+++ app/Http/Controllers/StickerPackController.php
@@ -11,15 +11,17 @@ class StickerPackController extends Controller
     public function getPack($code){
         $pack = StickerPack::where('code', $code)->first();
 
+        //START: Generating Thumbnails if not exist
         if(!empty($pack->stickers)){
             $root_folder = base_path().'/storage/app/public/sticker-packs/'.$code.'/';
             $width = 256;
             $stickers = json_decode($pack->stickers);
-            foreach($stickers as $sticker){
-                $original_file_path = $root_folder.$sticker;
-                $thumb_file_path = $root_folder.$width.'__'.$sticker;
 
-                if(!file_exists($thumb_file_path)){
+            if(!file_exists($root_folder.$width.'__'.$stickers[0])){
+                foreach($stickers as $sticker){
+                    $original_file_path = $root_folder.$sticker;
+                    $thumb_file_path = $root_folder.$width.'__'.$sticker;
+
                     //Resize image for desired width
                     if (mime_content_type ( $original_file_path ) == 'image/gif') {
                         $im = new \Imagick($original_file_path);
@@ -40,6 +42,7 @@ class StickerPackController extends Controller
                 }
             }
         }
+        //END: Generating Thumbnails if not exist
 
         return view('stickerpack.details')->with(['pack' => $pack]);
     }
