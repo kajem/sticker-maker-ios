716202175c7f289950217c4ee029d243d69bf8d5
Kajemul Haque <kajemsust@gmail.com>
2020-08-18 17:43:08 +0600

    date is removed from api password

--- app/Http/Middleware/ApiAuth.php
+++ app/Http/Middleware/ApiAuth.php
@@ -17,30 +17,19 @@ class ApiAuth
      */
     public function handle($request, Closure $next)
     {
-        $today = date('Ymd');
-        if(empty($request->header('UserName')) || empty($request->header('Password')) || strpos($request->header('Password'),$today) === false){
+        if(empty($request->header('UserName')) || empty($request->header('Password'))){
             return $this->errorOutput();
         }
-        $password = str_replace($today, "", $request->header('Password'));
 
         $usernames = ['7liAmLyJLU05u4Dfy9CYKpXWqXaFtMD6EU6d2uGfgB2qi7'];
         $passwords = ['54jdKKFG8u9JwACVbLbHk5GsT8h5nckaMGeQEntV8zRdFIRxYHeIO'];
-        $username_index = array_search($request->header('UserName'), $usernames);
 
+        $username_index = array_search($request->header('UserName'), $usernames);
         if($username_index === false)
             return $this->errorOutput();
 
-        $cond = $password !== $passwords[0] ? "note matched" : "matched";
-
-        if($password !== $passwords[$username_index])
+        if($request->header('Password') !== $passwords[$username_index])
             return $this->errorOutput();
-
-        // $project_password = ApiPassword::select('id')->where('username', $request->header('UserName'))
-        //                     ->where('password', $password)
-        //                     ->first();
-        // if(empty($project_password->id)){
-        //     return $this->errorOutput();
-        // }
 
         return $next($request);
     }
