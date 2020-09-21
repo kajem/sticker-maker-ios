922e61189583963ce52a87834d80c8546d6a6bac
Kajemul Haque <kajemsust@gmail.com>
2020-09-21 17:59:55 +0600

    css

--- resources/views/frontpage/sections/sticker-pack.blade.php
+++ resources/views/frontpage/sections/sticker-pack.blade.php
@@ -4,14 +4,15 @@
             <div class="row">
                 <div class="col-sm-12 pt-5 pb-4">
                     <h2 class="title text-center pb-3">Cool and Fancy Sticker Gallery</h2>
-
+                    @php $count = 1; @endphp
                     @foreach($items as $item)
                         @php
                             $thumb_arr = explode('/', $item->thumb);
                             $thumb = $asset_base_url.'items/'.$item->code.'/200__'.end($thumb_arr);
+
                         @endphp
                         <a href="{{url('pack/braincraft/'.$item->code)}}">
-                            <div class="card sticker-bg-{{rand(1, 21)}} text-center">
+                            <div class="card sticker-bg-{{$count++}} text-center">
                                 <span class="badge badge-pill badge-warning">{{$item->total_sticker}} Stickers</span>
                                 <div class="image text-center">
                                     <img class="card-img-top" src="{{$thumb}}" alt="Card image cap">
