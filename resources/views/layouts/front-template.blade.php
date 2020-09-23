01bbbec60c67b4be18a6434aa7f04d1e12714dd1
Kajemul Haque <kajemsust@gmail.com>
2020-09-23 14:57:06 +0600

    google tag manager code implemented

--- resources/views/layouts/front-template.blade.php
+++ resources/views/layouts/front-template.blade.php
@@ -23,12 +23,12 @@
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-TS4NLPL');</script>
+        })(window,document,'script','dataLayer','GTM-TK3ZJCV');</script>
     <!-- End Google Tag Manager -->
 </head>
 <body>
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TS4NLPL"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TK3ZJCV"
                   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 <div class="position-ref full-height flex-center">
