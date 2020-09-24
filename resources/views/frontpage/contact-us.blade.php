0e15b06b98e5c30d2ebcf4aeab814c6c37dfb8cd
Kajemul Haque <kajemsust@gmail.com>
2020-09-24 18:45:31 +0600

    js issue

--- resources/views/frontpage/contact-us.blade.php
+++ resources/views/frontpage/contact-us.blade.php
@@ -6,7 +6,6 @@
             </div>
         </div>
         <form id="contact-form" action="#" method="post">
-            {{csrf_field()}}
             <div class="form-group">
                 <div class="row">
                     <div class="col-sm-6">
