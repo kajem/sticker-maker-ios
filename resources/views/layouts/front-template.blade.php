f8ba07997ec102950b4a7aff244664fd50ed65e8
Kajemul Haque <kajemsust@gmail.com>
2020-09-27 18:54:34 +0600

    bug fixed

--- resources/views/layouts/front-template.blade.php
+++ resources/views/layouts/front-template.blade.php
@@ -8,8 +8,8 @@
 
     <!-- Favicons -->
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/favicons/favicon-16x16.png')}}" />
-    <link rel="icon" type="image/png" sizes="32x32" href="asset('images/favicons/favicon-32x32.png')}}" />
-    <link rel="icon" type="image/png" sizes="96x96" href="asset('images/favicons/favicon-96x96.png')}}" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('images/favicons/favicon-32x32.png')}}" />
+    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('images/favicons/favicon-96x96.png')}}" />
 
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Font Awesome Icons -->
