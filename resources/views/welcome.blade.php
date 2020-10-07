9e1717d8f0ec0845012d0a42a72ad1729febfb43
Kajemul Haque <kajemsust@gmail.com>
2020-10-07 12:31:05 +0600

    meta title and description added for all pages.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.front-template',
 [
     'page_title' => 'Personalize your own stickers easily!',
-    'meta_title' => 'Sticker Maker for Social Apps',
-    'meta_keywords' => 'sticker maker online, sticker maker app, sticker maker app for whatsapp,  sticker maker app for facebook,  sticker maker app for messenger,  sticker maker app for imessage, sticker maker app iphone, sticker maker at home, create a sticker maker, buy a sticker maker, download a sticker maker, use a sticker maker, how does a sticker maker work, sticker maker a whatsapp, sticker maker best, sticker maker company, sticker maker custom, sticker maker digital, sticker maker editor, sticker maker emoji, sticker maker for whatsapp, sticker maker for business, sticker maker from photo, sticker maker ios, sticker maker in store, sticker maker instagram, sticker maker imessage, sticker maker ipad, sticker maker kids, sticker maker on mac, sticker maker pro, quick sticker maker, quote sticker maker, sticker maker remove background, sticker maker studio, sticker maker software, sticker maker site, sticker maker store, sticker maker stickers, sticker makers, sticker maker text, sticker maker transparent background, sticker maker usa, sticker maker website, sticker maker whatsapp, sticker maker youtube, sticker making, sticker. maker, sticker maker ios 14',
-    'meta_description' => "Create your stickers for WhatsApp, Facebook, Messenger, iMessage! Share the stickers in social apps and end the boring chat! Add your stickers on iMessage and WhatsApp or download as PNG photos! Share single stickers or entire sticker packs!"
+    'meta_title' => 'Sticker Maker for Messenger Apps',
+    'meta_description' => "Use Sticker Maker to create memes or stickers for Whatsapp, iMessege, other social apps. Get animated stickers and cool stickers from chat sticker packs!"
 ])
 @section('content')
     @include('frontpage.hero-area')
