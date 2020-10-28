247bbecaea926d7661e8071ae8dd1e55961e4ce9
Kajemul Haque <kajemsust@gmail.com>
2020-10-28 12:22:51 +0600

    Add new item changed to 'Add new pack' in admin panel

--- resources/views/admin/item/list.blade.php
+++ resources/views/admin/item/list.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.admin-template', ['pageTitle' => 'All Packs', 'backButtonText' => '<i class="fas fa-plus"></i> Add new item', 'backButtonURL' => url('item/add'), 'showBackButton' => true])
+@extends('layouts.admin-template', ['pageTitle' => 'All Packs', 'backButtonText' => '<i class="fas fa-plus"></i> Add new pack', 'backButtonURL' => url('item/add'), 'showBackButton' => true])
 @section('content')
     <!-- Main content -->
     <div class="content">
