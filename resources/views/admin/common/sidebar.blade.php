50f61f816c12eb63fb824aa3ea614e41062ebd87
Kajemul Haque <kajemsust@gmail.com>
2020-12-01 18:36:49 +0600

    login page - mobile menu icon removed, in admin-> mobile view ->logout link added

--- resources/views/admin/common/sidebar.blade.php
+++ resources/views/admin/common/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{url('/dashboard')}}" class="brand-link">
-        <img src="{{ asset("/bower_components/admin-lte/dist/img/AdminLTELogo.png") }}" alt="AdminLTE Logo"
+        <img src="/images/logo.png" alt="Sticker Maker Logo"
              class="brand-image img-circle elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">StickerMaker</span>
@@ -149,6 +149,15 @@
                             </a>
                         </li>
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i>
+                        <p>Logout</p>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
+                    </a>
                 </li>
             </ul>
         </nav>
