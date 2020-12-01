c9ea1d8de72ab02aeba1a20258b6afe66c52f6f5
Kajemul Haque <kajemsust@gmail.com>
2020-12-01 14:29:50 +0600

    pagination layout issue in how to use stickermaker articles list

--- resources/views/post/how-to-use-sm/list.blade.php
+++ resources/views/post/how-to-use-sm/list.blade.php
@@ -35,7 +35,7 @@
                                 <div class="col-sm-6 mt-4">
                                     @include('post.how-to-use-sm.card')
                                 </div>
-                        @if($i % 2 == 0 || $i >= ($posts->perpage() - 1) )
+                        @if($i % 2 == 0 || $i >= $posts->perpage() || $i >= $posts->count() )
                             </div>
                         @endif
                         @php
