ee710e5592c759675692bdd4384b07cddc62e534
Kajemul Haque <kajemsust@gmail.com>
2020-12-24 19:04:24 +0600

    Home page item made dynamic with static_value name from admin panel

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 use App\Item;
 use App\StaticValue;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
@@ -16,7 +17,9 @@ class WelcomeController extends Controller
         $home_sticker_packages = StaticValue::select('value')->where('name', 'website_home_page_packages')->first();
         $items = Item::select('name', 'slug', 'code', 'thumb', 'author', 'total_sticker')
                      ->whereIn('id', explode(",", $home_sticker_packages->value))
-                     ->where('status', 1)->get();
+                     ->where('status', 1)
+                     ->orderByRaw(DB::raw("FIELD(id, $home_sticker_packages->value)"))
+                     ->get();
         //dd($home_sticker_packages->value);
         $data = [
             'asset_base_url' => config('app.asset_base_url'),
